00956f43d36fdd63aedec5e8efd4f5d82803c660
Vanessa Nava <vnava@csumb.edu>
2018-02-08 04:34:08 +0000

    Add all homework files

---
+++ home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset = "utf-8"/>
+    <title>Computer Science: Artificial intelligence </title>
+    
+</head>
+<body id = "homeBody">
+    <header>
+       <h1>Artificial Intelligence</h1> 
+       
+       <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+    </header>
+    
+    <nav>
+        <hr width = "50%" />
+        <a href = "home.php">Home</a>
+        <a href = "risk.html">Risks</a>
+        <a href = "benefits.html">Benefits</a>
+        <a href = "projects.html">AI Projects</a>
+        
+    </nav>
+    <br/><br/>
+    
+    <main>
+      <figure id = "picture">
+          
+      </figure>  
+        
+      
+      <div id = "aiInfo">
+          <aside>
+              <h2>What is Artificial Intelligence?</h2>
+              <p>Artificial Intelligence, better known as AI, is the intelligence displayed by machines.</p>
+              <p>Without human knowlege AI would not be possible because it is the humans who write the </p>
+              <p>code to propt the computer how to think or react.</p>        
+              <p>Many people have their opinion about AI innovations; some believe the concept is a good thing</p>
+              <p>while others think there is much risk on the ideas.</p>
+              </br></br>
+              
+              <img src = "/Homework1/img/artificial.jpg" alt = "human and robot holding hands"/>
+              </br></br></br></br>
+              
+          </aside>
+      </div>
+      
+    </main>
+    
+    <footer id = "home">
+        <hr>
+            CST 336 Internet Programming. 2017 &copy; Nava <br />
+            <strong>Disclamer:</strong> The infornmation on this page is fictitous. <br />
+            It is used for academic purpose only.
+    </footer>
+    
+
+</body>
+</html>
